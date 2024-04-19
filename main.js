8e7bb78c66a6198bf60da583b85fae154b34cd35
Максим Шабельник <maks2015611@gmail.com>
2024-04-19 22:56:23 +0300

    init project

---
+++ main.js
@@ -0,0 +1,24 @@
+import { Telegraf, Markup } from 'telegraf'
+import {message} from "telegraf/filters";
+
+const token = '6962938579:AAE_Zi1vuUL5Fr3gKXSIT7xN9hAQVhh_mTY'
+const webAppUrl = 'https://angular-app-tg.web.app/'
+
+const bot = new Telegraf(token)
+
+bot.command('start', (ctx) => {
+    ctx.reply(
+        'Добро пожаловать! Нажмите на кнопку ниже, чтобы запустить приложение',
+        Markup.inlineKeyboard([
+            Markup.button.webApp('Отправить сообщение', `${webAppUrl}/feedback`),
+            Markup.button.webApp('Новое сообщение', `${webAppUrl}/feedback`),
+        ])
+    )
+})
+
+bot.on(message('web_app_data'), async (ctx) => {
+    const data = ctx.webAppData.data.json()
+    ctx.reply(`Ваше сообщение: ${data?.feedback}` ?? 'empty message')
+})
+
+bot.launch()
