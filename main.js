588eebc452932288710faef75bfe2e3ef5f4fc8a
Максим Шабельник <maks2015611@gmail.com>
2024-04-20 03:43:09 +0300

    псевдо авторизация

--- main.js
+++ main.js
@@ -5,29 +5,38 @@ const token = '6962938579:AAE_Zi1vuUL5Fr3gKXSIT7xN9hAQVhh_mTY'
 const webAppUrl = 'https://angular-app-tg.web.app/'
 
 const bot = new Telegraf(token)
+// Список пользователей и их чат id
+const allowedChatIds = [
+    802052372,
+    688437402,
+];
 
 bot.command('start', (ctx) => {
-    ctx.reply(
-        'Добро пожаловать! Нажмите на кнопку ниже, чтобы запустить приложение',
-        Markup.inlineKeyboard([
-            Markup.button.webApp('Отправить сообщение', `${webAppUrl}/feedback`),
-            Markup.button.webApp('Новое сообщение', `${webAppUrl}/feedback`),
-        ])
-    )
+    // Проверяем chat id пользователя
+    if (allowedChatIds.includes(ctx.from.id)) {
+        // Если chat id пользователя в списке разрешенных, отправляем сообщение и клавиатуру
+        ctx.reply(
+            'Добро пожаловать! Нажмите на кнопку ниже, чтобы запустить приложение',
+            Markup.keyboard([
+                Markup.button.webApp('Отправить сообщение', `${webAppUrl}/feedback`),
+                Markup.button.webApp('Новое сообщение', `${webAppUrl}/feedback`),
+            ])
+        );
 
-    // Отправка дополнительного приветственного сообщения
-    ctx.reply(
-        `Привет!\n\n` +
-        `Обучалки:\n` +
-        `• Первая обучалка: ${webAppUrl}education` + // Ссылка на первую обучалку
-        `• Вторая обучалка\n`
-    );
-
-})
-
-bot.on(message('web_app_data'), async (ctx) => {
-    const data = ctx.webAppData.data.json()
-    ctx.reply(`Ваше сообщение: ${data?.feedback}` ?? 'empty message')
-})
-
+        // Отправка дополнительного приветственного сообщения
+        ctx.reply(
+            `Привет!\n\n` +
+            `Обучалки:\n` +
+            `• <a href="${webAppUrl}education">Быстрое начало</a>\n` +
+            `• <a href="${webAppUrl}education">Как редактировать профиль?</a>\n` +
+            `• <a href="${webAppUrl}education">Как назначить удобные для встреч дни?</a>\n` +
+            `• <a href="${webAppUrl}education">Как изменить дни для встреч?</a>\n` +
+            `• <a href="${webAppUrl}education">Как удалить профиль?</a>\n` ,
+            { parse_mode: 'HTML' }
+        );
+    } else {
+        // Если chat id пользователя не в списке разрешенных, отправляем сообщение с его chat id
+        ctx.reply(`Извините, ваш chat id: ${ctx.from.id}, не имеет доступа к этому боту.`);
+    }
+});
 bot.launch()
