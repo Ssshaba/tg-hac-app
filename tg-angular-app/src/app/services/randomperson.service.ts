84828237fe1f5652c032577ef9e1973098df2bda
D3NCH11K <daniilotreshko@mail.ru>
2024-04-20 15:31:52 +0300

    randomperson

--- tg-angular-app/src/app/services/randomperson.service.ts
+++ tg-angular-app/src/app/services/randomperson.service.ts
@@ -6,10 +6,10 @@ import { Injectable } from '@angular/core';
 })
 export class RandompersonService {
   private users = [
-    { id: 1, name: 'John', role: 'Developer', photo: 'avatar.png' },
-    { id: 2, name: 'Alice', role: 'Designer', photo: 'avatar.png' },
-    { id: 3, name: 'Bob', role: 'Manager', photo: 'avatar.png' },
-    { id: 4, name: 'Emma', role: 'Tester', photo: 'avatar.png' }
+    { id: 1, name: 'John', role: 'Developer', age: '45', photo: 'avatar.png' },
+    { id: 2, name: 'Alice', role: 'Designer', age: '23', photo: 'avatar.png' },
+    { id: 3, name: 'Bob', role: 'Manager', age: '33', photo: 'avatar.png' },
+    { id: 4, name: 'Emma', role: 'Tester', age: '32', photo: 'avatar.png' }
   ];
 
   constructor() { }
