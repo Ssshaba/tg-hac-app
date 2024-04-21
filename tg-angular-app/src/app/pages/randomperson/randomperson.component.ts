43e12a600f9168190b05581249a9b05432ab8e49
D3NCH11K <daniilotreshko@mail.ru>
2024-04-21 13:05:25 +0300

    write to random

--- tg-angular-app/src/app/pages/randomperson/randomperson.component.ts
+++ tg-angular-app/src/app/pages/randomperson/randomperson.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ChangeDetectionStrategy, ChangeDetectorRef } from '@angular/core';
 import { Router } from '@angular/router';
 import { RandompersonService } from '../../services/randomperson.service';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-randomperson',
@@ -10,8 +11,9 @@ import { RandompersonService } from '../../services/randomperson.service';
 })
 export class RandompersonComponent implements OnInit {
   user: any;
+  chatLink: string | null = null; // Ссылка на чат
 
-  constructor(private router: Router, private randompersonService: RandompersonService, private cdr: ChangeDetectorRef) { }
+  constructor(private router: Router, private randompersonService: RandompersonService, private cdr: ChangeDetectorRef, private http: HttpClient) { }
 
   ngOnInit() {
     this.generateRandomUser();
@@ -21,10 +23,28 @@ export class RandompersonComponent implements OnInit {
     try {
       this.user = await this.randompersonService.getRandomUser();
       console.log('Данные пользователя:', this.user);
+      console.log('ID пользователя:', this.user.tgId);
+
+      this.getChatLink(this.user.tgId); // Вызываем функцию для получения ссылки на чат после получения ID пользователя
 
       this.cdr.detectChanges();
     } catch (error) {
       console.error('Ошибка при получении случайного пользователя:', error);
+    }
+  }
+
+  async getChatLink(chatId: number) {
+    try {
+      const response: any = await this.http.get(`https://api.telegram.org/bot6962938579:AAE_Zi1vuUL5Fr3gKXSIT7xN9hAQVhh_mTY/getChat?chat_id=${chatId}`).toPromise();
+      if (response.ok) {
+        this.chatLink = 'https://t.me/' + response.result.username;
+        console.log('Ссылка на чат:', this.chatLink);
+        this.cdr.detectChanges();
+      } else {
+        console.error('Ошибка при получении ссылки на чат:', response);
+      }
+    } catch (error) {
+      console.error('Ошибка при получении ссылки на чат:', error);
     }
   }
 
@@ -42,5 +62,13 @@ export class RandompersonComponent implements OnInit {
 
   goToMainPage() {
     this.router.navigate(['/']);
+  }
+
+  goToChat() {
+    if (this.chatLink) {
+      window.location.href = this.chatLink;
+    } else {
+      console.error('Ссылка на чат не доступна');
+    }
   }
 }
